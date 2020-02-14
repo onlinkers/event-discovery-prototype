faac98cca09a0db6ecdfc5a002202d2e46992a1d
Chris <chris.791.ix@gmail.com>
2020-02-14 11:47:11 -0800

    started event details page, global variables now available, defined color schemes in variables.scss

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 import Welcome from './views/Welcome'
 import SignUp from './views/SignUp'
 import Discover from './views/Discover'
+import EventPage from './views/EventPage'
 
 /* FOR TESTING */
 import createEventModal from './components/createEventModal'
@@ -34,6 +35,11 @@ const router = new Router({
       name: 'create-event',
       component: createEventModal
     },
+    {
+      path: '/eventpage',
+      name: 'event-page',
+      component: EventPage
+    }
   ]
 })
 
