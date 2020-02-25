759f5f27ec506cae6fb4ef5b36d16dff8f7451c2
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2020-02-25 01:52:20 -0800

    make form validation rules

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -46,22 +46,13 @@
   </div>
 </template>
 <script>
+import { rules } from '../utils'
 export default {
   data() {
     return {
       inputEmail: '',
       inputPassword: '',
-      rules: {
-        required: value => !!value || 'Required.',
-        counter: value => value.length <= 20 || 'Max 20 characters',
-        email: value => {
-          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          return pattern.test(value) || 'Invalid e-mail.'
-        },
-      },
-
-      email: '',
-
+      rules: rules,
     }
   }
 }
