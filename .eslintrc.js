c688fbeaf9f0629cb24f5a8fc0e7c971179cef51
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2020-02-26 22:47:47 -0800

    overwrite env-dependent and basic rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,57 @@ module.exports = {
         'plugin:vue/recommended',
     ],
     rules: {
+        // Disable console logs on production branch
+        'no-console': process.env.NODE_ENV === 'prod' ? 2 : 1,
+        'no-debugger': process.env.NODE_ENV === 'prod' ? 2 : 1,
+
+        // Order of component details (default)
+        'vue/order-in-components': [1, {
+            'order': [
+              'el',
+              'name',
+              'parent',
+              'functional',
+              ['delimiters', 'comments'],
+              ['components', 'directives', 'filters'],
+              'extends',
+              'mixins',
+              'inheritAttrs',
+              'model',
+              ['props', 'propsData'],
+              'data',
+              'computed',
+              'watch',
+              'LIFECYCLE_HOOKS',
+              'methods',
+              ['template', 'render'],
+              'renderError'
+            ]
+          }],
+          // Order in component attributes (default)
+          "vue/attributes-order": ["warning", {
+            "order": [
+              "DEFINITION",
+              "LIST_RENDERING",
+              "CONDITIONALS",
+              "RENDER_MODIFIERS",
+              "GLOBAL",
+              "UNIQUE",
+              "TWO_WAY_BINDING",
+              "OTHER_DIRECTIVES",
+              "OTHER_ATTR",
+              "EVENTS",
+              "CONTENT"
+            ],
+            "alphabetical": false
+          }],
+          'vue/max-attributes-per-line': [1, {
+            'singleline': 2,
+            'multiline': {
+              'max': 1,
+              'allowFirstLine': false
+            }
+          }],
     },
     parserOptions: {
         parser: 'babel-eslint'
