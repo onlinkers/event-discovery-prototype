4fdfdda9b8ee3131ad0fff7a2352d1fd4fbd73f2
Chris <chris.791.ix@gmail.com>
2020-03-02 17:25:39 -0800

    added prettier configuration to lint to avoid conflict

--- src/views/EventPage.vue
+++ src/views/EventPage.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="page-container">
-    <div class="nav-btns">
-      <img src="../assets/icons/arrow-left-light.svg" alt>
-      <img src="../assets/icons/share-light.svg" alt>
-    </div>
-    <div class="title-container">
-      <h1 class="event-title">Burning Man</h1>
-      <v-btn fab text>
-        <img src="../assets/icons/like-button.svg" alt>
-      </v-btn>
-      <img src alt>
+    <NavBtns />
+    <div class="background-img">
+      <img
+        src="https://images.unsplash.com/photo-1533105079780-92b9be482077?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80"
+        class=""
+      >
     </div>
   </div>
 </template>
 
-<script lang="ts">
+<script lang="js">
+import NavBtns from '../components/navButtons.vue';
+import eventData from '../assets/js/eventData.js';
+
 export default {
+  components: {
+    NavBtns
+  },
+  props: {
+    eventData
+  },
   data() {
     return {
-      hasLiked: false
     };
   },
   methods: {
@@ -30,36 +34,6 @@ export default {
 
 <style lang="scss" scoped>
 .page-container {
-  background-image: url("../assets/sample-img.png");
-  height: 100vh;
-  width: 100%;
-
-  .nav-btns {
-    position: fixed;
-    top: 2%;
-    width: 80%;
-    height: 10%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-content: center;
-  }
-
-  .title-container {
-    position: absolute;
-    top: 40%;
-    width: 80%;
-    left: 10%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-content: center;
-
-    .event-title {
-      font-family: "Josefin Sans", sans-serif;
-      font-size: 2.2em;
-      letter-spacing: -0.05em;
-    }
-  }
+  @include page-container-middle;
 }
 </style>
