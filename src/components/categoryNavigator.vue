7fb2e0259c7722c7961ae3d83af2c4193e160d18
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2020-03-29 22:21:20 -0700

    differenciate active and inactive

--- src/components/categoryNavigator.vue
+++ src/components/categoryNavigator.vue
@@ -1,8 +1,21 @@
 <template>
     <div class="CategoryScroller">
         <v-chip
-            v-for="categoryKey in EVENT_CATEGORIES_KEYS"
+            v-for="categoryKey in activeCategoryKeys"
             :key="categoryKey"
+            class="active-chip"
+            color="#2351f4"
+            ripple
+            small
+            @click="removeCategory(categoryKey)"
+        >
+        {{ EVENT_CATEGORIES[categoryKey] }}
+        </v-chip>
+        <v-chip
+            v-for="categoryKey in inactiveCategoryKeys"
+            :key="categoryKey"
+            class="inactive-chip"
+            color="transparent"
             ripple
             small
             @click="addCategory(categoryKey)"
@@ -14,6 +27,7 @@
 
 <script>
 import { EVENT_CATEGORIES, EVENT_CATEGORIES_KEYS } from '../constants';
+import { filterExistingCategoryKeys } from '../utils';
 
 export default {
   name: "CategoryNavigator",
@@ -25,8 +39,20 @@ export default {
   data() {
     return {
       EVENT_CATEGORIES,
-      EVENT_CATEGORIES_KEYS
+      EVENT_CATEGORIES_KEYS,
+      activeCategoryKeys: [],
+      inactiveCategoryKeys: EVENT_CATEGORIES_KEYS,
     };
+  },
+  watch: {
+    categories() {
+        this.activeCategoryKeys = this.categories;
+        this.inactiveCategoryKeys = filterExistingCategoryKeys(EVENT_CATEGORIES_KEYS, this.categories);
+    }
+  },
+  mounted() {
+    this.activeCategoryKeys = this.categories;
+    this.inactiveCategoryKeys = filterExistingCategoryKeys(EVENT_CATEGORIES_KEYS, this.categories);
   },
   methods: {
     addCategory(categoryKey) {
@@ -36,30 +62,52 @@ export default {
       newCategories.push(categoryKey);
       queries.categories = newCategories.join(',');
       this.$router.push({ name: 'discover', query: { ...queries } })
+    },
+    removeCategory(categoryKey) {
+      // Hack to overcome the vue 'duplicate' route problem
+      const queries = Object.assign({}, this.$route.query);
+      const newCategories = this.categories.filter((category) => category !== categoryKey);
+      queries.categories = newCategories.join(',');
+      this.$router.push({ name: 'discover', query: { ...queries } })
     }
   }
 }
 </script>
 <style lang="scss" scoped>
   .CategoryScroller {
-    width: 40vw;
     display: flex;
     flex-direction: row;
     position: absolute;
     top: 21%;
     left: 50%;
+    margin-left: 15vw;
     transform: translateX(-50%);
     overflow-x: scroll;
+    width: 100vw;
+
     @media screen and (min-width: 700px){
+      left: 50%;
+      transform: translateX(-50%);
+      width: 40vw;
       top: 20%;
-    }
-    @media screen and (max-width: 850px) {
-      width: 70vw;
+      margin-left: 0;
+
+      .v-chip {
+        margin-right: 0.3em;
+      }
     }
 
     .v-chip {
       overflow: visible;
       cursor: pointer;
+      margin-right: 0.1em;
+    }
+
+    .active-chip {
+      color: white;
+    }
+    .inactive-chip {
+      opacity: 0.5;
     }
   }
 
--- src/utils/_utils/eventFilters.js
+++ src/utils/_utils/eventFilters.js
@@ -6,3 +6,12 @@ export const filterByCategories = (events, filterArray = []) => {
         return filterArray.some((category) => event.categories.indexOf(EVENT_CATEGORIES[category]) > -1)
     });
 }
+
+
+// Filters out an array of categories which exist in the filterArray
+export const filterExistingCategoryKeys = (categories, filterArray = []) => {
+    if(!filterArray.length) return categories
+    return categories.filter((category) => {
+        return !filterArray.includes(category)
+    });
+}
