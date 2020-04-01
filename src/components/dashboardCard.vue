618758364eb81e14e8cded4d8fa90eb29cee71fd
Chris <chris.791.ix@gmail.com>
2020-03-31 18:41:31 -0700

    restyled cards

--- src/components/dashboardCard.vue
+++ src/components/dashboardCard.vue
@@ -1,5 +1,5 @@
-<template>
-  <v-card
+x<template>
+  <!-- <v-card
     max-width="400"
     class="mx-auto"
   >
@@ -36,32 +36,134 @@
         </v-row>
       </v-item-group>
     </v-container>
-  </v-card>
+  </v-card>-->
+  <div class="card-container">
+    <img src="../assets/icons/like-button.svg" class="like-btn" alt />
+    <img src="../assets/media/test-card-img.png" class="card-cover-img" alt />
+    <div class="card-details">
+      <h2 class="card-title card-details__detail">Island Cruise</h2>
+      <div class="card-tags card-details__detail">
+        <p>
+          <span>Tag1</span>
+          <span>Tag2</span>
+          <span>Tag3</span>
+        </p>
+      </div>
+      <div class="card-location card-details__detail">
+        <img src="../assets/icons/event-page/location.svg" alt />
+        <p>Location</p>
+      </div>
+      <div class="card-participation card-details__detail">
+        <img src="../assets/icons/event-page/people.svg" alt />
+        <p>144</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    data: () => ({
+export default {
+  data() {
+    return {
       items: [
         {
-          src: 'backgrounds/bg.jpg',
+          src: "backgrounds/bg.jpg"
         },
         {
-          src: 'backgrounds/md.jpg',
+          src: "backgrounds/md.jpg"
         },
         {
-          src: 'backgrounds/bg-2.jpg',
+          src: "backgrounds/bg-2.jpg"
         },
         {
-          src: 'backgrounds/md2.jpg',
-        },
+          src: "backgrounds/md2.jpg"
+        }
       ],
-      selected: [],
-    }),
+      selected: []
+    };
   }
+};
 </script>
 
 
 <style lang="scss" scoped>
+.card-container {
+  position: absolute;
+  width: 290px;
+  height: 350px;
+  display: flex;
+  flex-direction: column;
+  padding: 1em;
+  background: #f5f5f6;
+  border-radius: 10px;
 
+  .like-btn {
+    position: absolute;
+    top: 7%;
+    right: 7%;
+  }
+
+  .card-cover-img {
+    object-fit: cover;
+    border-radius: 10px;
+    margin-bottom: 1em;
+  }
+
+  .card-details {
+    .card-details__detail {
+      margin-bottom: 0.25em;
+    }
+
+    .card-title {
+      @include card-title;
+    }
+
+    .card-tags {
+      p {
+        @include card-tag;
+        margin: 0;
+
+        span {
+          margin-right: 1em;
+        }
+      }
+    }
+
+    .card-location {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-start;
+      align-items: center;
+
+      img {
+        width: 1.25em;
+        height: 1.25em;
+      }
+
+      p {
+        @include card-detail;
+        margin: 0;
+        padding-left: 0.5em;
+      }
+    }
+
+    .card-participation {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-start;
+      align-items: center;
+
+      img {
+        width: 1.25em;
+        height: 1.25em;
+      }
+
+      p {
+        @include card-detail;
+        margin: 0;
+        padding-left: 0.5em;
+      }
+    }
+  }
+}
 </style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,15 +1,18 @@
 <template>
-    <div class="Dashboard">
-        <SearchBar />
-        <mq-layout mq="desktop">
-         <ExploreBar />
-        </mq-layout>
-        <div class="event-list">
+  <div class="Dashboard">
+    <SearchBar />
+    <mq-layout mq="desktop">
+      <ExploreBar />
+    </mq-layout>
+    <!-- <div class="event-list">
            <h2>Popular Events World, or any date</h2>
            <DashboardCard />
 
-        </div>
+    </div>-->
+    <div class="event-list">
+      <DashboardCard />
     </div>
+  </div>
 </template>
 
 <script>
@@ -20,43 +23,39 @@ import DashboardCard from "../components/dashboardCard";
 import { mapActions, mapState } from "vuex";
 
 export default {
-    name: "Dashboard",
-    components: {
-        SearchBar,
-        ExploreBar,
-        DashboardCard
-    },
-    data() {
-        return {
-            
-        };
-    },
-    computed: {
-        ...mapState('events', {
-            events: state => state.general
-        })
-    },
-    created() {
-        // QUERY logic should be handled here
-        // it will be easier to handle map re-loads, async calls, loading, etc
-        this.queryAllEvents()
-    },
-    method: {
-        ...mapActions('events', [
-        'queryAllEvents'
-        ]),
-    }
-}
+  name: "Dashboard",
+  components: {
+    SearchBar,
+    ExploreBar,
+    DashboardCard
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState("events", {
+      events: state => state.general
+    })
+  },
+  created() {
+    // QUERY logic should be handled here
+    // it will be easier to handle map re-loads, async calls, loading, etc
+    this.queryAllEvents();
+  },
+  method: {
+    ...mapActions("events", ["queryAllEvents"])
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
 .event-list {
-    position: absolute;
-    top: 20%;
-    left: 5%;
-    width: 90%;
-    display: flex;
-    flex-direction: row;
+  position: absolute;
+  top: 30%;
+  left: 5%;
+  width: 90%;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
 }
 </style>
